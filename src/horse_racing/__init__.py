6a17bbb88686b17ae99c20287ce48747ae2e068f
coba-yu <yukob.formal@gmail.com>
2025-01-03 02:49:46 +0900

    feat(race): usecase/race_schedule.py - get_race_result

--- src/horse_racing/__init__.py
+++ src/horse_racing/__init__.py
@@ -1,3 +1,6 @@
+import polars as pl
+from tqdm import tqdm
+
 from horse_racing.core.chrome import ChromeDriver
 from horse_racing.usecase.race_schedule import RaceScheduleUsecase
 
@@ -10,6 +13,19 @@ def main() -> None:
     month = 1
     race_dates = race_schedule_usecase.get_race_dates(year=year, month=month)
     print(race_dates)
+
+    race_date_to_ids_dict = {}
     for race_date in race_dates:
-        race_ids = race_schedule_usecase.get_race_ids(race_date=race_date)
-        print(f"{race_date=}, {race_ids=}")
+        race_ids_per_date = race_schedule_usecase.get_race_ids(race_date=race_date)
+        print(f"{race_date=}, {race_ids_per_date=}")
+        race_date_to_ids_dict[race_date] = race_ids_per_date
+
+    all_df: pl.DataFrame | None = None
+    for race_date, race_ids in race_date_to_ids_dict.items():
+        for race_id in tqdm(race_ids, desc=f"{race_date=}"):
+            df = race_schedule_usecase.get_race_result(race_id=race_id, race_date=race_date)
+            if all_df is None:
+                all_df = df
+            else:
+                all_df = pl.concat([df, all_df])
+    print(all_df)
--- src/horse_racing/usecase/race_schedule.py
+++ src/horse_racing/usecase/race_schedule.py
@@ -1,6 +1,10 @@
 import os.path
 import re
+from io import StringIO
+from time import sleep
 
+import pandas as pd
+import polars as pl
 from horse_racing.core.chrome import ChromeDriver
 from horse_racing.core.html import get_html, get_soup
 
@@ -14,8 +18,31 @@ def extract_race_date(href: str) -> str | None:
 
 
 class RaceScheduleUsecase:
+    race_result_columns = [
+        "着 順",
+        "枠",
+        "馬 番",
+        "馬名",
+        "性齢",
+        "斤量",
+        "騎手",
+        "タイム",
+        "着差",
+        "人 気",
+        "単勝 オッズ",
+        "後3F",
+        "コーナー 通過順",
+        "厩舎",
+        "馬体重 (増減)",
+    ]
+
     def __init__(self, driver: ChromeDriver) -> None:
         self.driver = driver
+
+    def _make_tmp_dir(self, sub_dir: str) -> str:
+        tmp_dir = os.path.join("data", "tmp", "html", sub_dir)
+        os.makedirs(tmp_dir, exist_ok=True)
+        return tmp_dir
 
     @staticmethod
     def get_race_dates(year: int, month: int) -> list[str]:
@@ -36,9 +63,7 @@ class RaceScheduleUsecase:
         return race_dates
 
     def get_race_ids(self, race_date: str) -> list[str]:
-        tmp_dir = os.path.join("data", "tmp", "html", "race_list")
-        os.makedirs(tmp_dir, exist_ok=True)
-
+        tmp_dir = self._make_tmp_dir(sub_dir="race_list")
         tmp_html_path = os.path.join(tmp_dir, f"{race_date}.html")
         if os.path.isfile(tmp_html_path):
             with open(tmp_html_path, "r") as f:
@@ -65,3 +90,23 @@ class RaceScheduleUsecase:
             race_ids.append(race_id)
 
         return race_ids
+
+    def get_race_result(self, race_id: str, race_date: str) -> pl.DataFrame:
+        tmp_dir = self._make_tmp_dir(sub_dir=os.path.join("race_results", f"race_date={race_date}"))
+        tmp_html_path = os.path.join(tmp_dir, f"{race_id}.html")
+
+        if os.path.isfile(tmp_html_path):
+            with open(tmp_html_path, "r") as f:
+                html = f.read()
+        else:
+            url = f"https://race.netkeiba.com/race/result.html?race_id={race_id}"
+            html = get_html(url)
+            sleep(1.0)
+            with open(tmp_html_path, "w") as f:
+                f.write(html)
+
+        pdf_list = pd.read_html(StringIO(html), converters={c: str for c in self.race_result_columns})
+        if len(pdf_list) < 1:
+            return pl.DataFrame()
+        df = pl.from_pandas(pdf_list[0])
+        return df.with_columns(race_id=pl.lit(race_id), race_date=pl.lit(race_date))
