34fd8d7a1469574a977f521c69055177a9f763d3
coba-yu <yukob.formal@gmail.com>
2025-01-03 15:07:02 +0900

    fix: add decorator get_html_with_cache

--- src/horse_racing/core/chrome.py
+++ src/horse_racing/core/chrome.py
@@ -2,19 +2,21 @@ from time import sleep
 
 from selenium.webdriver import Chrome, ChromeOptions
 
-from horse_racing.core.user_agent import random_choice_user_agent
+from horse_racing.core.html import DEFAULT_SLEEP_SECONDS, random_choice_user_agent
 
 
 class ChromeDriver:
-    def __init__(self) -> None:
+    def __init__(self, sleep_seconds: float = DEFAULT_SLEEP_SECONDS) -> None:
         self._options = ChromeOptions()
         self._options.add_argument("--headless")
         self._options.add_argument("--no-sandbox")
         self._options.add_argument("--disable-gpu")
 
+        self._sleep_seconds = sleep_seconds
+
     def get_page_source(self, url: str) -> str:
         self._options.add_argument(f"--user-agent={random_choice_user_agent()}")
         with Chrome(options=self._options) as driver:
             driver.get(url=url)
-            sleep(1.0)
+            sleep(self._sleep_seconds)
             return str(driver.page_source)
--- src/horse_racing/core/html.py
+++ src/horse_racing/core/html.py
@@ -1,8 +1,18 @@
+import os
 import random
+from functools import wraps
+from pathlib import Path
+from time import sleep
+from typing import Callable, Concatenate, ParamSpec
 
 import requests
 from bs4 import BeautifulSoup
 
+from horse_racing.core.logging import logger
+
+P = ParamSpec("P")
+
+DEFAULT_SLEEP_SECONDS = 30.0
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
@@ -23,9 +33,53 @@ def random_choice_user_agent() -> str:
     return random.choice(USER_AGENTS)
 
 
+def make_cache_dir(sub_dir: str) -> str:
+    tmp_dir = os.path.join("data", "cache", "html", sub_dir)
+    os.makedirs(tmp_dir, exist_ok=True)
+    return tmp_dir
+
+
+def get_html_with_cache(
+    func: Callable[Concatenate[str, P], str],
+) -> Callable[..., str]:
+    @wraps(func)
+    def wrapper(
+        url: str,
+        *args: P.args,
+        sleep_seconds: float = DEFAULT_SLEEP_SECONDS,
+        cache_sub_path: str | Path | None = None,
+        **kwds: P.kwargs,
+    ) -> str:
+        if cache_sub_path is not None:
+            cache_sub_dir = os.path.dirname(cache_sub_path)
+            cache_dir = make_cache_dir(sub_dir=cache_sub_dir)
+
+            cache_file_name = os.path.basename(cache_sub_path)
+            cache_path = os.path.join(cache_dir, cache_file_name)
+            logger.info(f"Try to get cache: {cache_path}")
+            if os.path.isfile(cache_path):
+                logger.info("Cache found")
+                with open(cache_path, "r") as f:
+                    return f.read()
+            logger.info("Cache not found")
+
+        html = func(url, *args, **kwds)
+        sleep(sleep_seconds)
+
+        if cache_sub_dir is not None:
+            logger.info(f"Save cache: {cache_path}")
+            with open(cache_path, "w") as f:
+                f.write(html)
+
+        return html
+
+    return wrapper
+
+
+@get_html_with_cache
 def get_html(url: str) -> str:
     headers = {"user-agent": random_choice_user_agent()}
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers, timeout=30)
     r.raise_for_status()
 
     # workaround
