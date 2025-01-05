58069ac8203345820be135a69f9a22848153272f
coba-yu <yukob.formal@gmail.com>
2025-01-05 13:30:31 +0900

    fix(action): resource.tf - rm pubsub service

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,12 +1,25 @@
-# Enable Pub/Sub API
-resource "google_project_service" "pubsub" {
-  service            = "pubsub.googleapis.com"
-  disable_on_destroy = false
+#==========================#
+# IAM with Service Account #
+#==========================#
+
+data "google_service_account" "yukob_horse_racing" {
+  account_id = "yukob-horse-racing"
 }
+
+resource "google_project_iam_member" "gcp_iam_member" {
+  for_each = toset(var.gcp_iam_roles)
+  project  = var.google_project
+  role     = each.value
+  member   = "serviceAccount:${data.google_service_account.yukob_horse_racing.email}"
+}
+
+#==================#
+# data preparation #
+#==================#
 
 resource "google_pubsub_topic" "horse_racing_data" {
   name       = "horse-racing-data"
-  depends_on = [google_project_service.pubsub]
+  depends_on = [google_project_iam_member.gcp_iam_member]
 }
 
 resource "google_cloud_scheduler_job" "horse_racing_data" {
