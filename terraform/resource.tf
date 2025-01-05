931b10f5c9e0dcb5162dbeec8b2fc1807b3ae08b
coba-yu <yukob.formal@gmail.com>
2025-01-05 13:30:31 +0900

    fix(action): resource.tf - rm pubsub service

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,12 +1,5 @@
-# Enable Pub/Sub API
-resource "google_project_service" "pubsub" {
-  service            = "pubsub.googleapis.com"
-  disable_on_destroy = false
-}
-
 resource "google_pubsub_topic" "horse_racing_data" {
   name       = "horse-racing-data"
-  depends_on = [google_project_service.pubsub]
 }
 
 resource "google_cloud_scheduler_job" "horse_racing_data" {
