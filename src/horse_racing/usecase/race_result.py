66c66a19bf0c962b8cc49df339ff0e7d4166a47f
coba-yu <yukob.formal@gmail.com>
2025-03-15 08:43:25 +0900

    fix: _RESULT_COLUMN_RENAME_DICT

--- src/horse_racing/usecase/race_result.py
+++ src/horse_racing/usecase/race_result.py
@@ -6,6 +6,25 @@ import polars as pl
 
 from horse_racing.infrastructure.netkeiba.race_result import RaceResultNetkeibaRepository
 
+# raw -> renamed
+_RESULT_COLUMN_RENAME_DICT = {
+    "着 順": "rank",
+    "枠": "frame",
+    "馬 番": "horse_number",
+    "馬名": "horse_name",
+    "性齢": "gender_age",
+    "斤量": "total_weight",
+    "騎手": "jockey_name",
+    "タイム": "goal_time",
+    "着差": "goal_diff",
+    "人 気": "popular",
+    "単勝 オッズ": "odds",
+    "後3F": "last_3f_time",
+    "コーナー 通過順": "corner_rank",
+    "厩舎": "trainer_name",
+    "馬体重 (増減)": "horse_weight_and_diff",
+}
+
 
 class RaceResultUsecase:
     def __init__(
@@ -32,9 +51,14 @@ class RaceResultUsecase:
         for data in self.race_result_repository.get_iter():
             sub_dir = f'race_date={data["race_date"]}'
 
-            table_pdf_list = pd.read_html(StringIO(data["html"]))
+            table_pdf_list = pd.read_html(
+                StringIO(data["html"]),
+                converters={c: str for c, _ in _RESULT_COLUMN_RENAME_DICT.items()},
+            )
 
             result_df = pl.from_pandas(table_pdf_list[0])
+            result_df = result_df.rename(_RESULT_COLUMN_RENAME_DICT)
+
             result_dir = data_dir / "race_results" / sub_dir
             result_dir.mkdir(parents=True, exist_ok=True)
             result_df.write_parquet(result_dir / f'{data["race_id"]}.parquet')
