bc868c22a72dc17e03e70e8390c0b985386688e7
coba-yu <yukob.formal@gmail.com>
2025-03-15 11:25:34 +0900

    feat: add race_date / race_id / race info

--- src/horse_racing/usecase/race_result.py
+++ src/horse_racing/usecase/race_result.py
@@ -1,11 +1,15 @@
+import re
 from io import StringIO
 from pathlib import Path
 from traceback import format_exc
+from typing import Any
 
 import pandas as pd
 import polars as pl
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+from horse_racing.core.html import get_soup
 from horse_racing.core.logging import logger
 from horse_racing.infrastructure.netkeiba.race_result import RaceResultNetkeibaRepository
 
@@ -27,6 +31,59 @@ _RESULT_COLUMN_RENAME_DICT = {
     "厩舎": "trainer_name",
     "馬体重 (増減)": "horse_weight_and_diff",
 }
+
+
+def extract_race_info(soup: BeautifulSoup) -> dict[str, Any]:
+    race_info: dict[str, Any] = {}
+
+    # => "1R"
+    race_list_name_box = soup.find("div", class_="RaceList_NameBox")
+    race_num_tag = race_list_name_box.select_one(".RaceList_Item01 .RaceNum")
+    if race_num_tag is None:
+        race_info["race_number"] = None
+    else:
+        race_info["race_number"] = race_num_tag.get_text(strip=True)
+
+    # => "3歳未勝利"
+    race_name_tag = race_list_name_box.select_one(".RaceList_Item02 .RaceName")
+    if race_num_tag is None:
+        race_info["race_name"] = None
+    else:
+        race_info["race_name"] = race_name_tag.get_text(strip=True)
+
+    # => "10:10発走 / ダ1400m (左) / 天候:晴 / 馬場:良"
+    race_data_1_tag = race_list_name_box.select_one(".RaceList_Item02 .RaceData01")
+    if race_data_1_tag is None:
+        race_info_texts = []
+    else:
+        race_info_texts = re.sub(r"\s+", "", race_data_1_tag.get_text(strip=True)).split("/")
+
+    for i, k in enumerate(("start_at", "distance", "weather", "field_condition")):
+        if len(race_info_texts) >= i + 1:
+            race_info[k] = race_info_texts[i]
+        else:
+            race_info[k] = None
+
+    return dict(**race_info)
+
+
+def convert_html_to_dataframe(html: str, race_date: str, race_id: str) -> pl.DataFrame:
+    # base dataframe
+    table_pdf_list = pd.read_html(
+        StringIO(html),
+        converters={c: str for c, _ in _RESULT_COLUMN_RENAME_DICT.items()},
+    )
+    df = pl.from_pandas(table_pdf_list[0])
+    df = df.rename(_RESULT_COLUMN_RENAME_DICT)
+
+    df = df.with_columns(race_id=pl.lit(race_id), race_date=pl.lit(race_date))
+
+    # extract info
+    soup = get_soup(html)
+    race_info = extract_race_info(soup=soup)
+    df = df.with_columns([pl.lit(v).alias(k) for k, v in race_info.items()])
+
+    return df
 
 
 class RaceResultUsecase:
@@ -61,13 +118,7 @@ class RaceResultUsecase:
             sub_dir = f"race_date={race_date}"
 
             try:
-                table_pdf_list = pd.read_html(
-                    StringIO(data["html"]),
-                    converters={c: str for c, _ in _RESULT_COLUMN_RENAME_DICT.items()},
-                )
-
-                result_df = pl.from_pandas(table_pdf_list[0])
-                result_df = result_df.rename(_RESULT_COLUMN_RENAME_DICT)
+                result_df = convert_html_to_dataframe(html=data["html"], race_date=race_date, race_id=race_id)
 
                 result_dir = data_dir / "race_results" / sub_dir
                 result_dir.mkdir(parents=True, exist_ok=True)
