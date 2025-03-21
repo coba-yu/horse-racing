459ef6b3034b6e968f08042b60936132e67dc310
coba-yu <yukob.formal@gmail.com>
2025-03-21 19:32:06 +0900

    feat: model bucket

--- terraform/resource.tf
+++ terraform/resource.tf
@@ -59,24 +59,6 @@ resource "google_pubsub_topic" "horse_racing_data" {
   depends_on = [google_project_iam_member.gcp_iam_member]
 }
 
-# resource "google_cloud_scheduler_job" "horse_racing_data" {
-#   name     = "horse-racing-data"
-#   schedule = "0 22 * * *"
-#
-#   pubsub_target {
-#     topic_name = google_pubsub_topic.horse_racing_data.id
-#     data = base64encode(
-#       jsonencode({
-#         message = "trigger scheduled run"
-#       })
-#     )
-#   }
-#   depends_on = [
-#     google_project_iam_member.gcp_iam_member,
-#     google_pubsub_topic.horse_racing_data,
-#   ]
-# }
-
 resource "google_cloud_run_v2_job" "scrape_netkeiba_job" {
   name                = "scrape-netkeiba-job"
   location            = var.region
@@ -129,6 +111,28 @@ resource "google_cloud_run_v2_job" "scrape_netkeiba_job_race_dates" {
   ]
 }
 
+resource "google_workflows_workflow" "scrape_netkeiba" {
+  name                = "scrape-netkeiba"
+  region              = var.region
+  deletion_protection = false
+  service_account     = "projects/${var.google_project}/serviceAccounts/${google_service_account.yukob_horse_racing_workflow.email}"
+  source_contents     = file("../workflows/scrape-netkeiba.yaml")
+  depends_on = [
+    google_service_account.yukob_horse_racing_workflow,
+    google_cloud_run_v2_job.scrape_netkeiba_job,
+  ]
+}
+
+# ============== #
+# model training #
+# ============== #
+
+resource "google_storage_bucket" "horse_racing_models" {
+  name          = "yukob-horse-racing-models"
+  location      = var.region
+  force_destroy = false
+}
+
 resource "google_cloud_run_v2_job" "train_lgb_job" {
   name                = "train-lgb-job"
   location            = var.region
@@ -155,66 +159,3 @@ resource "google_cloud_run_v2_job" "train_lgb_job" {
     google_storage_bucket.netkeiba_htmls,
   ]
 }
-
-resource "google_workflows_workflow" "scrape_netkeiba" {
-  name                = "scrape-netkeiba"
-  region              = var.region
-  deletion_protection = false
-  service_account     = "projects/${var.google_project}/serviceAccounts/${google_service_account.yukob_horse_racing_workflow.email}"
-  source_contents     = file("../workflows/scrape-netkeiba.yaml")
-  depends_on = [
-    google_service_account.yukob_horse_racing_workflow,
-    google_cloud_run_v2_job.scrape_netkeiba_job,
-  ]
-}
-
-# data "archive_file" "gcf_src_scheduled_pipeline" {
-#   type        = "zip"
-#   source_dir  = "${path.module}/../src/horse_racing/app/functions/scheduled_pipeline"
-#   output_path = "${path.module}/../src/horse_racing/app/functions/scheduled_pipeline.zip"
-# }
-#
-# resource "google_storage_bucket_object" "scheduled_pipeline_function_src" {
-#   name       = "functions/scheduled_pipeline/${data.archive_file.gcf_src_scheduled_pipeline.output_md5}.zip"
-#   bucket     = data.google_storage_bucket.horse_racing_pipelines.name
-#   source     = data.archive_file.gcf_src_scheduled_pipeline.output_path
-#   depends_on = [google_project_iam_member.gcp_iam_member]
-# }
-#
-# resource "google_cloudfunctions2_function" "scheduled_pipeline_function" {
-#   name     = "scheduled-pipeline-horse-racing-data"
-#   location = var.region
-#
-#   build_config {
-#     runtime     = "python312"
-#     entry_point = "subscribe"
-#     source {
-#       storage_source {
-#         bucket = data.google_storage_bucket.horse_racing_pipelines.name
-#         object = google_storage_bucket_object.scheduled_pipeline_function_src.name
-#       }
-#     }
-#   }
-#
-#   service_config {
-#     timeout_seconds  = 540
-#     available_memory = "1Gi"
-#     available_cpu    = "1"
-#     environment_variables = {
-#       REGION = var.region
-#     }
-#     service_account_email = var.service_account_email
-#   }
-#
-#   event_trigger {
-#     event_type   = "google.cloud.pubsub.topic.v1.messagePublished"
-#     pubsub_topic = google_pubsub_topic.horse_racing_data.id
-#     retry_policy = "RETRY_POLICY_DO_NOT_RETRY"
-#   }
-#
-#   depends_on = [
-#     google_project_iam_member.gcp_iam_member,
-#     google_storage_bucket_object.scheduled_pipeline_function_src,
-#     google_pubsub_topic.horse_racing_data,
-#   ]
-# }
