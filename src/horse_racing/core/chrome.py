a86aedd6ea8e92f57916ff30a50f18ea5e86e690
coba-yu <yukob.formal@gmail.com>
2025-05-31 01:24:26 +0900

    fix: retry ChromeDriver.get_page_source

--- src/horse_racing/core/chrome.py
+++ src/horse_racing/core/chrome.py
@@ -1,5 +1,6 @@
 from time import sleep
 
+import tenacity
 from selenium.webdriver import Chrome, ChromeOptions
 
 from horse_racing.core.html import DEFAULT_SLEEP_SECONDS, random_choice_user_agent
@@ -15,8 +16,13 @@ class ChromeDriver:
 
         self._sleep_seconds = sleep_seconds
 
+    @tenacity.retry(
+        stop=tenacity.stop_after_attempt(10),
+        wait=tenacity.wait_exponential_jitter(initial=1.0, max=60.0),
+    )  # type: ignore[misc]
     def get_page_source(self, url: str, skip_sleep: bool = False) -> str:
         self._options.add_argument(f"--user-agent={random_choice_user_agent()}")
+
         with Chrome(options=self._options) as driver:
             driver.get(url=url)
             if not skip_sleep:
